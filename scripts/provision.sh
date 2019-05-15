a7e4bbebf9fefe404289d6dd315666cc71e59c96
Martin Hristov <martinhristov@Martins-MacBook-Pro.local>
2019-05-15 09:28:55 +0300

    Adding initial content

---
+++ scripts/provision.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+which curl docker make ruby || {
+  sudo apt-get update
+  sudo apt-get install -y curl docker.io make ruby
+  sudo usermod -a -G docker ubuntu
+  sudo gem install rubyzip
+  pushd ~/buildDir
+  sudo gem install bundler 
+  bundler install
+  popd
+  sudo apt-get clean
+
+}
+
+which packer || {
+  cd /usr/local/bin
+  curl -sSL https://raw.githubusercontent.com/kikitux/download-hashicorp-tools/master/download-packer.rb | sudo ruby
+}
